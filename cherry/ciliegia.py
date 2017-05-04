821a60da0b211c3ac7739a28e6ec19f7198b2c5e
Stefano Scipioni <scipio.it@gmail.com>
2017-05-04 12:31:13 +0200

    segmentation ?

--- cherry/ciliegia.py
+++ cherry/ciliegia.py
@@ -1,21 +1,86 @@
 # -*- coding:utf-8 -*-
 
+import numpy as np
 import cv2
 import time
+from cherry.lib import cvutil
 
 def draw_text(img, text, y=75, x=10, color=(255,255,0)):
     cv2.putText(img, str(text), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color)
 
+class Ciliegie:
+    def __init__(self):
+        self.ciliegie = []
+
+    def update(self, ciliegie):
+        """
+        aggancia le nuove ciliegie a quelle presenti
+        """
+        pass
 
 class Ciliegia:
-    def __init__(self):
+    _area_min = 50 # normalizzata e in millesimi
+    _aspect_ratio_level = 0.4 # percentuale di scostamento massima dal quadrato
+
+    def __init__(self, contour, h_parent, w_parent, taratura=28.0):
+        """
+            la ciliegia ai bordi dell'immagine ha un calibro un 10% inferiore rispetto al centro
+            a causa della distanza dall'obbiettivo della camera
+        """
+        self.contour = contour
+        self.h_parent = h_parent
+        self.mirino = None
         self.x = 0
         self.y = 0
-        self.area = 0
+
         self.vs = []
         self.last = time.time()
         self.fired = False
 
+        self.rect = cv2.minAreaRect(contour)
+        xy,wh,theta = self.rect
+        self.w,self.h = wh
+        self.x,self.y = map(int,xy) # centro della ciliegia
+        self.area_norm = 1000*self.w*self.h/(h_parent*w_parent) # normalizziamo alla dimensione del parente e in millesimi
+        self.y_norm = 1.0*self.y/h_parent
+        self.calibro = 0
+        if self.w > 0:
+            self.aspect_ratio = 1.0*self.h/self.w # il quadrato ha 1
+        else:
+            self.aspect_ratio = 0
+
+    def set_mirino(self, mirino):
+        self.mirino = mirino
+        self.calibro = round(10.0*self.mirino.taratura*max(self.w, self.h)/self.h_parent)/10
+
+        if self.calibro > mirino.calibro-2: # quello che non è passato dal calibro precedente
+            distance = 100.0*(self.y-mirino.y)/self.h_parent
+            #print distance
+            if 0 <= distance <= 2:
+                mirino.fire()
+                print "FIRED"
+
+    def draw(self, img):
+        cv2.circle(img, (self.x, self.y), 4, cvutil.yellow, 2)
+        box = cv2.boxPoints(self.rect) # cv2.boxPoints(rect) for OpenCV 3.x
+        box = np.int0(box)
+        cv2.drawContours(img, [box], -1, cvutil.green, 1)
+        cv2.putText(img, str(self.calibro), (self.x, self.y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, cvutil.yellow)
+
+    def is_valid(self):
+        if not self.w or not self.h:
+            return False
+        if self.area_norm < self._area_min:
+            #print("BAD area %s" % self.area_norm)
+            return False
+        if abs(1-self.aspect_ratio) > self._aspect_ratio_level:
+            #print("BAD aspect ratio %s" % self.aspect_ratio)
+            return False
+        if self.w*self.h < 10: # troppo piccolo
+            return False
+        return True
+
+    '''
     def update(self, img, hull, x, y, area, y_min):
         posizione = float(y-y_min)/y_min
         if posizione > 0.8:
@@ -46,3 +111,4 @@ class Ciliegia:
             return
         print(" FIRE v=%d vs=%s" % (v, self.vs))
         self.vs = []
+    '''
--- cherry/process.py
+++ cherry/process.py
@@ -8,23 +8,41 @@ import time
 import argparse
 
 from cherry.lib import cvutil
-from cherry.ciliegia import Ciliegia
+from cherry.ciliegia import Ciliegia, Ciliegie
 
 
 class Mirino:
-    def __init__(self, y=0.5, delta=0.20):
-        self.ciliegia = Ciliegia()
+    def __init__(self, y=0.5, delta=0.20, calibro=28):
+        self.calibro = calibro
+        self.fired = False
 
-    def update(self, img, y=0.5, delta=0.2):
+    def update(self, img, y=0.5, delta=0.2, taratura=1.0):
         """
-            y_min e y_max hanno lo zero nella parte bassa dell'immagine
+            y_min e y_max hanno lo zero nella parte alta dell'immagine
         """
-        height, width = img.shape[:2]
+        self.img = img
+        self.taratura = taratura
+        height, self.width = img.shape[:2]
 
-        self.y_max = int(height*y+height*delta)
-        self.y_min = int(height*y-height*delta)
-        cv2.rectangle(img, (0,height-self.y_max), (width,height-self.y_min), cvutil.yellow, 2)
+        self.y_top = int(height*y-height*delta)
+        self.y_bottom = int(height*y+height*delta)
+
+        #self.y_max = int(height*y+height*delta)
+        #self.y_min = int(height*y-height*delta)
+        self.y = self.y_bottom+(self.y_top-self.y_bottom)/2
+        cv2.line(img, (0, self.y), (self.width, self.y), cvutil.red, 1)
+        cv2.rectangle(img, (0,self.y_top), (self.width,self.y_bottom), cvutil.yellow, 2)
+        cv2.putText(img, str(self.calibro), (50, self.y-30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, cvutil.yellow)
+        #cv2.rectangle(img, (0,height-self.y_max), (width,height-self.y_min), cvutil.yellow, 2)
+        #cv2.line(img, (0,height-self.y), (width, height-self.y), cvutil.red, 1)
         #return (cy_min, cy_max)
+
+    def fire(self):
+        self.fired = True
+        cv2.line(self.img, (0, self.y), (self.width, self.y), cvutil.red, 4)
+
+    def contains(self, ciliegia):
+        return self.y_top <= ciliegia.y <= self.y_bottom
 
 class Detector:
     def __init__(self, show=False, filename=''):
@@ -42,16 +60,20 @@ class Detector:
         #self.saturation_max = self.window.add_trackbar('saturation_max', 1, 255, default=255)
         self.value_min = self.window.add_trackbar('value_min', 0, 255, default=0)
         #self.value_max = self.window.add_trackbar('value_max', 1, 255, default=255)
-        self.w = self.window.add_trackbar('w', 0.2, 0.8, step=0.01, default=0.4)
+        self.w = self.window.add_trackbar('w', 0.2, 0.8, step=0.01, default=0.41)
         self.w_offset = self.window.add_trackbar('w_offset', 0.0, 0.2, step=0.01, default=0.08)
-        self.mirino1_y = self.window.add_trackbar('mirino1_y', 0.1, 0.8, step=0.01, default=0.35)
-        self.mirino2_y = self.window.add_trackbar('mirino2_y', 0.1, 0.8, step=0.01, default=0.80)
         self.mirino_h = self.window.add_trackbar('mirino_h', 0.1, 0.8, step=0.01, default=0.12)
+        self.mirino1_y = self.window.add_trackbar('mirino1_y', 0.1, 0.8, step=0.01, default=0.7)
+        self.mirino2_y = self.window.add_trackbar('mirino2_y', 0.1, 0.8, step=0.01, default=0.2)
+
+        self.mirino1_cal = self.window.add_trackbar('mirino1_cal', 150, 230, step=1, default=183)
+        self.mirino2_cal = self.window.add_trackbar('mirino2_cal', 150, 230, step=1, default=200)
 
         self.window_result = cvutil.Window('result') #, size=(160,600))
 
-        self.mirino1 = Mirino()
-        self.mirino2 = Mirino()
+        self.mirino1 = Mirino(calibro=28)
+        self.mirino2 = Mirino(calibro=26)
+        self.cilieigie = Ciliegie()
 
     def erode(self, img, kernel=5):
         kernel_ = np.ones((kernel,kernel),np.uint8)
@@ -79,9 +101,48 @@ class Detector:
             (self.hue_max.getInt(), 255, 255),
             )
         result = cv2.bitwise_and(img, img, mask=mask1)
-        return result
+        return result, mask1
 
-    def calculate(self, img_out, img_mask):
+    def get_ciliegie_grey(self, image):
+        """
+        torna delle ciliegie valide
+        """
+        grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        (buff, threshold) = cv2.threshold(grey, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        cv2.imshow('black', threshold)
+
+        if cvutil.major >= 3:
+            buff, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        else:
+            contours,hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if not contours:
+            return
+
+        #cy_min, cy_max = self.mirino(img_out)
+
+        hierarchy = hierarchy[0]
+
+        #y=110
+        ciliegie =  []
+        for component in zip(contours, hierarchy):
+
+            currentContour, currentHierarchy = component
+            cv2.drawContours(image, [currentContour], -1, cvutil.blu, 2)
+            #if currentHierarchy[3] >= 0:
+            #    continue
+            #cv2.drawContours(image, [currentContour], -1, cvutil.green, 2)
+            ciliegia = Ciliegia(currentContour, *image.shape[:2])
+            if ciliegia.is_valid():
+                ciliegie.append(ciliegia)
+        return ciliegie
+
+
+    def get_ciliegie(self, img_mask):
+        """
+        torna delle ciliegie valide
+        """
+        #grey = cv2.cvtColor(img_mask, cv2.COLOR_BGR2GRAY)
+        #(buff, threshold) = cv2.threshold(grey, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
         if cvutil.major >= 3:
             buff, contours, hierarchy = cv2.findContours(img_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         else:
@@ -89,31 +150,25 @@ class Detector:
         if not contours:
             return
 
-        cy_min, cy_max = self.mirino(img_out)
+        #cy_min, cy_max = self.mirino(img_out)
 
         hierarchy = hierarchy[0]
 
         #y=110
+        ciliegie =  []
         for component in zip(contours, hierarchy):
-            currentContour = component[0]
-            currentHierarchy = component[1]
+            currentContour, currentHierarchy = component
             if currentHierarchy[3] >= 0:
                 continue
 
-            rect = cv2.minAreaRect(currentContour)
-            xy,wh,theta = rect
-            w,h = wh
-            if w*h < 10: # troppo piccolo
-                return
-            x,y = xy # centro della ciliegia
+            ciliegia = Ciliegia(currentContour, *img_mask.shape[:2])
+            if ciliegia.is_valid():
+                ciliegie.append(ciliegia)
 
             # calcoliamo
 
             # visualizziamo il rect
-            box = cv2.boxPoints(rect) # cv2.boxPoints(rect) for OpenCV 3.x
-            box = np.int0(box)
-            cv2.drawContours(img_out, [box], -1, cvutil.yellow, 2)
-
+            '''
             area = cv2.contourArea(currentContour)
             area = area/100.0
 
@@ -134,6 +189,8 @@ class Detector:
                 #    area = round(area*1.2)
                 self.ciliegia.update(img_out, hull, cx, cy, area, cy_min)
                 #y += 50
+            '''
+        return ciliegie
 
     def crop(self, frame):
         slice=self.w.get()
@@ -158,12 +215,24 @@ class Detector:
 
             original_show = original.copy()
             frame = self.crop(original_show)
-            self.mirino1.update(original_show, y=self.mirino1_y.get(), delta=self.mirino_h.get())
-            self.mirino2.update(original_show, y=self.mirino2_y.get(), delta=self.mirino_h.get())
+            #frame = cv2.GaussianBlur(frame, (5,5), 0)
+            self.mirino1.update(original_show, y=self.mirino1_y.get(), delta=self.mirino_h.get(), taratura=self.mirino1_cal.get())
+            self.mirino2.update(original_show, y=self.mirino2_y.get(), delta=self.mirino_h.get(), taratura=self.mirino2_cal.get())
 
-            frame = self.filter_color(frame)
+            # sistema basato sui colori
+            #frame, mask = self.filter_color(frame)
+            #ciliegie_nuove = self.get_ciliegie(mask)
 
-            #self.calculate(result, mask)
+            # sistema basato sulla forma
+            ciliegie_nuove = self.get_ciliegie_grey(frame)
+
+            for ciliegia_nuova in ciliegie_nuove:
+                if self.mirino1.contains(ciliegia_nuova):
+                    ciliegia_nuova.set_mirino(self.mirino1)
+                    ciliegia_nuova.draw(frame)
+                elif self.mirino2.contains(ciliegia_nuova):
+                    ciliegia_nuova.set_mirino(self.mirino2)
+                    ciliegia_nuova.draw(frame)
 
             fps = self.timer.fps
             if i%10:
@@ -179,8 +248,8 @@ class Detector:
                 pause = not pause
 
 
-
-            time.sleep(0.1)
+            i+=1
+            time.sleep(0.03)
 
         self.cap.release()
         cv2.destroyAllWindows()
